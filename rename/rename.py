fbdb02e918fdcd84a148f68279aef1274e620e05
VAlikV <alik.valiullin2002@yandex.ru>
2025-12-17 19:34:34 +0300

    .

--- rename/rename.py
+++ rename/rename.py
@@ -1,12 +1,12 @@
 import cv2
 import glob
 
-images = glob.glob("rename/input/*.png")
-count = 1
+images = glob.glob("rename/input/*.jpg")
+count = 139
 
 for i in images:
     image = cv2.imread(i)
 
-    cv2.imwrite("rename/output/" + str(count) + "_1.png", image)
+    cv2.imwrite("rename/output/f_" + str(count) + ".jpg", image)
 
     count += 1
